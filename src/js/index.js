940b443be206f939d8c113d3725e23481d78ded3
Ricky-L21 <71520397+Ricky-L21@users.noreply.github.com>
2021-03-18 01:03:41 -0400

    OVP

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,94 @@
-console.log("Connected")
+console.log("Connected");
+
+import { DOMSelectors } from "./Dom";
+import { menu, inStockItems, vegetarianOptions } from "./Menu";
+
+const init = function () {
+  const displayItems = menu.forEach((item) =>
+    DOMSelectors.displayContainer.insertAdjacentHTML(
+      "beforeend",
+      `<ul class="item-list">
+      <li class="item-name item-value">${item.name}</li>
+      <li class="item-price item-value"> $${item.price}</li>
+      <li class="item-vegetarian item-value">Vegetarian: ${item.vegetarian}</li>
+      <li class="item-image">
+        <img
+          class="item-image"
+          src="${item.img}"
+          alt=""
+        />
+      </li>
+      <li class="item-in-stock item-value">In Stock: ${item.inStock}</li>
+    </ul>`
+    )
+  );
+  DOMSelectors.fullMenuButton.addEventListener("click", function (e) {
+    DOMSelectors.displayContainer.innerHTML = "";
+    menu.forEach((item) => {
+      DOMSelectors.displayContainer.insertAdjacentHTML(
+        "beforeend",
+        `<ul class="item-list">
+      <li class="item-name item-value">${item.name}</li>
+      <li class="item-price item-value"> $${item.price}</li>
+      <li class="item-vegetarian item-value">Vegetarian: ${item.vegetarian}</li>
+      <li class="item-image">
+        <img
+          class="item-image"
+          src="${item.img}"
+          alt=""
+        />
+      </li>
+      <li class="item-in-stock item-value">In Stock: ${item.inStock}</li>
+    </ul>`
+      );
+    });
+  });
+  DOMSelectors.resetButton.addEventListener("click", function (e) {
+    DOMSelectors.displayContainer.innerHTML = "";
+    console.log("clear content works POG");
+  });
+  DOMSelectors.vegetarianButton.addEventListener("click", function (e) {
+    DOMSelectors.displayContainer.innerHTML = "";
+    vegetarianOptions.forEach((item) => {
+      DOMSelectors.displayContainer.insertAdjacentHTML(
+        "beforeend",
+        `<ul class="item-list">
+      <li class="item-name item-value">${item.name}</li>
+      <li class="item-price item-value"> $${item.price}</li>
+      <li class="item-vegetarian item-value">Vegetarian: ${item.vegetarian}</li>
+      <li class="item-image">
+        <img
+          class="item-image"
+          src="${item.img}"
+          alt=""
+        />
+      </li>
+      <li class="item-in-stock item-value">In Stock: ${item.inStock}</li>
+    </ul>`
+      );
+    });
+  });
+  DOMSelectors.inStockButton.addEventListener("click", function (e) {
+    DOMSelectors.displayContainer.innerHTML = "";
+    inStockItems.forEach((item) => {
+      DOMSelectors.displayContainer.insertAdjacentHTML(
+        "beforeend",
+        `<ul class="item-list">
+      <li class="item-name item-value">${item.name}</li>
+      <li class="item-price item-value"> $${item.price}</li>
+      <li class="item-vegetarian item-value">Vegetarian: ${item.vegetarian}</li>
+      <li class="item-image">
+        <img
+          class="item-image"
+          src="${item.img}"
+          alt=""
+        />
+      </li>
+      <li class="item-in-stock item-value">In Stock: ${item.inStock}</li>
+    </ul>`
+      );
+    });
+  });
+};
+
+init();
